b2470c52d344cf97e6d94ce9b1c33dca9539f654
Miguel Shin <miguelshin@gmail.com>
2019-11-12 19:16:41 -0600

    Add Reservations MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :flats
+  resources :flats do
+    resources :reservations, shallow: true #shortcut para sacar show, edit update y destroy de este nest
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
