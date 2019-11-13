3d1e43fb867b4a52be5f98b07e71d41d975e59eb
Miguel Shin <miguelshin@gmail.com>
2019-11-13 16:25:40 -0600

    Added InfoWindow PersonalMarkers SearchBar AutoComplete

--- app/javascript/packs/init_mapbox.js
+++ app/javascript/packs/init_mapbox.js
@@ -1,33 +1,36 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+  });
+}; 
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
+};
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
+  console.log("Hello world")
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-    if (mapElement) {
-      // [ ... ]
       const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach((marker) => {
-        new mapboxgl.Marker()
-          .setLngLat([ marker.lng, marker.lat ])
-          .addTo(map);
-      });
-      const fitMapToMarkers = (map, markers) => {
-        const bounds = new mapboxgl.LngLatBounds();
-        markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-        map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
-      };
-      
-      if (mapElement) {
-        // [ ... ]
-        fitMapToMarkers(map, markers);
-      }
-    }
+      addMarkersToMap(map, markers);
+      fitMapToMarkers(map, markers);
+      map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
   }
 };
 
