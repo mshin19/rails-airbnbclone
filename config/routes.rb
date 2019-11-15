6ed56dc7e27fa23e894aa1e2c9d61fe31b19d76a
Miguel Shin <miguelshin@gmail.com>
2019-11-14 18:25:54 -0600

    Fix Homepage Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'flats#index'
   resources :flats do
     resources :reservations, shallow: true #shortcut para sacar show, edit update y destroy de este nest
   end
